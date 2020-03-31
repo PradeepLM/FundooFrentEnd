183d282b872112d5479a15a5bbc55cbd1b3d82c4
PradeepLM <56271873+PradeepLM@users.noreply.github.com>
2020-03-31 10:09:14 +0530

    [pradeep] inigration for archieve notes

--- FundooWebApi/src/app/Components/archieve/archieve.component.ts
+++ FundooWebApi/src/app/Components/archieve/archieve.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Notes } from 'src/app/Model/notes.model';
 
 @Component({
   selector: 'app-archieve',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archieve.component.scss']
 })
 export class ArchieveComponent implements OnInit {
-
+  archievednotes:Notes[]
+  note:Notes=new Notes()
   constructor() { }
 
   ngOnInit(): void {
+    this.archievednotes=this.g.getarchieveNotesList()
   }
 
 }
